36d036970e7a055c457bf0f716671ca87f162c67
Kevin <116416787+homelabwithkevin@users.noreply.github.com>
2023-12-01 07:54:20 +0000

    create transcoding messages

--- stream_get_video.go
+++ stream_get_video.go
@@ -29,34 +29,42 @@ type MetaTags struct {
 	Value    string `json:"value,omitempty"`
 }
 
+type TranscodingMessages struct {
+	TimeStamp string `json:"timeStamp,omitempty"`
+	Level     int    `json:"level,omitempty"`
+	IssueCode int    `json:"issueCode,omitempty"`
+	Message   string `json:"message,omitempty"`
+	Value     string `json:"value,omitempty"`
+}
+
 type StreamVideo struct {
-	VideoLibraryId       int64      `json:"videoLibraryId,omitempty"`
-	Guid                 string     `json:"guid,omitempty"`
-	Title                string     `json:"title,omitempty"`
-	DateUploaded         string     `json:"dateUploaded,omitempty"`
-	Views                int64      `json:"views,omitempty"`
-	IsPublic             bool       `json:"isPublic,omitempty"`
-	Length               int32      `json:"length,omitempty"`
-	Status               int        `json:"status,omitempty"`
-	Framerate            int        `json:"Framerate,omitempty"`
-	Rotation             int        `json:"rotation,omitempty"`
-	Width                int32      `json:"width,omitempty"`
-	Height               int32      `json:"height,omitempty"`
-	AvailableResolutions string     `json:"availableResolutions,omitempty"`
-	ThumbnailCount       int32      `json:"thumbnailCount,omitempty"`
-	EncodeProgress       int32      `json:"encodeProgress,omitempty"`
-	StorageSize          int64      `json:"storageSize,omitempty"`
-	Captions             []Captions `json:"captions,omitempty"`
-	HasMP4Fallback       bool       `json:"hasMP4Fallback,omitempty"`
-	CollectionId         string     `json:"collectionId,omitempty"`
-	ThumbnailFileName    string     `json:"thumbnailFileName,omitempty"`
-	AverageWatchTime     int64      `json:"averageWatchTime,omitempty"`
-	TotalWatchTime       int64      `json:"totalWatchTime,omitempty"`
-	Category             string     `json:"category,omitempty"`
-	Chapters             []Chapters `json:"chapters,omitempty"`
-	Moments              []Moments  `json:"moments,omitempty"`
-	MetaTags             []MetaTags `json:"metaTags,omitempty"`
-	TranscodingMessages  string     `json:"transcodingMessages,omitempty"`
+	VideoLibraryId       int64                 `json:"videoLibraryId,omitempty"`
+	Guid                 string                `json:"guid,omitempty"`
+	Title                string                `json:"title,omitempty"`
+	DateUploaded         string                `json:"dateUploaded,omitempty"`
+	Views                int64                 `json:"views,omitempty"`
+	IsPublic             bool                  `json:"isPublic,omitempty"`
+	Length               int32                 `json:"length,omitempty"`
+	Status               int                   `json:"status,omitempty"`
+	Framerate            int                   `json:"Framerate,omitempty"`
+	Rotation             int                   `json:"rotation,omitempty"`
+	Width                int32                 `json:"width,omitempty"`
+	Height               int32                 `json:"height,omitempty"`
+	AvailableResolutions string                `json:"availableResolutions,omitempty"`
+	ThumbnailCount       int32                 `json:"thumbnailCount,omitempty"`
+	EncodeProgress       int32                 `json:"encodeProgress,omitempty"`
+	StorageSize          int64                 `json:"storageSize,omitempty"`
+	Captions             []Captions            `json:"captions,omitempty"`
+	HasMP4Fallback       bool                  `json:"hasMP4Fallback,omitempty"`
+	CollectionId         string                `json:"collectionId,omitempty"`
+	ThumbnailFileName    string                `json:"thumbnailFileName,omitempty"`
+	AverageWatchTime     int64                 `json:"averageWatchTime,omitempty"`
+	TotalWatchTime       int64                 `json:"totalWatchTime,omitempty"`
+	Category             string                `json:"category,omitempty"`
+	Chapters             []Chapters            `json:"chapters,omitempty"`
+	Moments              []Moments             `json:"moments,omitempty"`
+	MetaTags             []MetaTags            `json:"metaTags,omitempty"`
+	TranscodingMessages  []TranscodingMessages `json:"transcodingMessages,omitempty"`
 }
 
 // Get retrieves the Storage Zone with the given id.
