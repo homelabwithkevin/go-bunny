869d7b725024051c63bd80f4fef8e96195150268
Kevin <116416787+homelabwithkevin@users.noreply.github.com>
2023-12-01 13:04:42 +0000

    create initial test

---
+++ stream_get_video_test.go
@@ -0,0 +1,23 @@
+package bunny_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/simplesurance/bunny-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStreamGetVideoCRUD(t *testing.T) {
+	clt := bunny.NewClient("")
+
+	const videoLibraryId int64 = 179632
+	videoId := "e8117722-7af4-4723-aacf-36fb5f81496d"
+
+	videoInformation, err := clt.Stream.Get(context.Background(), videoLibraryId, videoId)
+	fmt.Print(videoInformation)
+	require.NoError(t, err, "video get failed")
+	assert.NotNil(t, videoInformation)
+}
