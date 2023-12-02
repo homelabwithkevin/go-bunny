ba4fad2fdfa959e19e75f04e54960c529ef1f7a1
Kevin <116416787+homelabwithkevin@users.noreply.github.com>
2023-12-02 01:38:12 +0000

    update videolibraryid to int64 and sprintf

--- stream_create_video.go
+++ stream_create_video.go
@@ -2,28 +2,29 @@ package bunny
 
 import (
 	"context"
+	"fmt"
 )
 
 // StreamCreateVideoOptions are the request parameters for the Stream Create Video API endpoint.
 //
 // Bunny.net API docs: https://docs.bunny.net/reference/video_createvideo
 type StreamCreateVideoOptions struct {
-	VideoLibraryId string `json:"VideoLibraryId,omitempty"`
+	VideoLibraryId int64  `json:"VideoLibraryId,omitempty"`
 	Title          string `json:"Title,omitempty"`
 	CollectionId   string `json:"CollectionId,omitempty"`
-	ThumbnailTime  int32  `json:"ThumbnailTime,omitempty"`
+	ThumbnailTime  *int32 `json:"ThumbnailTime,omitempty"`
 }
 
 // Create creates a new Video
 //
 // Bunny.net API docs: https://docs.bunny.net/reference/video_createvideo
 func (s *StreamService) Create(ctx context.Context, opts *StreamCreateVideoOptions) (*StreamVideo, error) {
-	videoLibraryId := opts.VideoLibraryId
+	videoLibraryId := fmt.Sprintf("/library/%d/videos", opts.VideoLibraryId)
 
 	return resourcePostWithResponse[StreamVideo](
 		ctx,
 		s.client,
-		"/library/"+videoLibraryId+"/videos",
+		videoLibraryId,
 		opts,
 	)
 }
