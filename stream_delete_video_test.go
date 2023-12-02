5b9a35558e572f755473d97210a99de6d345c304
Kevin <116416787+homelabwithkevin@users.noreply.github.com>
2023-12-02 13:17:50 -0500

    Create delete video test

---
+++ stream_delete_video_test.go
@@ -0,0 +1,30 @@
+package bunny_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/simplesurance/bunny-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStreamDeleteVideoCRUD(t *testing.T) {
+	// Stream Video Has Separate API Key than Account!
+	// https://docs.bunny.net/reference/api-overview
+	clt := bunny.NewClient("", "stream")
+
+	// Personal values, use your own
+	const videoLibraryId int64 = 179632
+	videoId = "1234"
+
+	videoOptions := bunny.StreamDeleteVideoOptions{
+		VideoLibraryId: videoLibraryId,
+		VideoId: videoId,
+	}
+
+	result, err := clt.Stream.Delete(context.Background(), &videoOptions)
+	fmt.Print(result)
+	require.NoError(t, err, "video create failed")
+}
