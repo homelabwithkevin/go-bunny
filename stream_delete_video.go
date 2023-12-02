44f84f47bc8a46942a001083342ab14b711a9d00
Kevin <116416787+homelabwithkevin@users.noreply.github.com>
2023-12-02 19:33:10 +0000

    Change to string and do resourceDelete call

--- stream_delete_video.go
+++ stream_delete_video.go
@@ -10,19 +10,14 @@ import (
 // Bunny.net API docs: https://docs.bunny.net/reference/video_deletevide
 type StreamDeleteVideoOptions struct {
 	VideoLibraryId int64  `json:"VideoLibraryId,omitempty"`
-	VideoId string  `json:"VideoId,omitempty"`
+	VideoId        string `json:"VideoId,omitempty"`
 }
 
 // Create creates a new Video
 //
 // Bunny.net API docs: https://docs.bunny.net/reference/video_deletevideo
-func (s *StreamService) Delete(ctx context.Context, opts *StreamCreateVideoOptions) (*StreamVideo, error) {
-	videoIdUrl := fmt.Sprintf("/library/%d/videos/%d", opts.VideoLibraryId, opts.VideoId)
+func (s *StreamService) Delete(ctx context.Context, opts *StreamDeleteVideoOptions) error {
+	videoIdUrl := fmt.Sprintf("/library/%d/videos/%s", opts.VideoLibraryId, opts.VideoId)
 
-	return resourcePostWithResponse[StreamVideo](
-		ctx,
-		s.client,
-		videoIdUrl,
-		opts,
-	)
+	return resourceDelete(ctx, s.client, videoIdUrl, opts)
 }
