05747b911e3a6519dd33332b2f8a3b8543fcc7f9
Kevin <116416787+homelabwithkevin@users.noreply.github.com>
2023-12-02 13:15:21 -0500

    Create stream_delete_video.go

---
+++ stream_delete_video.go
@@ -0,0 +1,28 @@
+package bunny
+
+import (
+	"context"
+	"fmt"
+)
+
+// StreamCreateVideoOptions are the request parameters for the Stream Create Video API endpoint.
+//
+// Bunny.net API docs: https://docs.bunny.net/reference/video_deletevide
+type StreamDeleteVideoOptions struct {
+	VideoLibraryId int64  `json:"VideoLibraryId,omitempty"`
+	VideoId int64  `json:"VideoId,omitempty"`
+}
+
+// Create creates a new Video
+//
+// Bunny.net API docs: https://docs.bunny.net/reference/video_deletevideo
+func (s *StreamService) Delete(ctx context.Context, opts *StreamCreateVideoOptions) (*StreamVideo, error) {
+	videoIdUrl := fmt.Sprintf("/library/%d/videos/%d", opts.VideoLibraryId, opts.VideoId)
+
+	return resourcePostWithResponse[StreamVideo](
+		ctx,
+		s.client,
+		videoIdUrl,
+		opts,
+	)
+}
