c80ebcf195193bde5ccde62f3e5e439c4e16eea2
Kevin <116416787+homelabwithkevin@users.noreply.github.com>
2023-12-03 08:09:33 -0500

    initial statistics

---
+++ stream_get_video_statistics.go
@@ -0,0 +1,38 @@
+package bunny
+
+import (
+	"context"
+	"fmt"
+)
+
+// StreamVideoStatistics represents the response of the Stream Statistics of the Stream Manage Videos API endpoint.
+//
+// Bunny.net API docs: https://docs.bunny.net/reference/video_getvideostatistics
+
+type StreamVideoStatistics struct {
+	ViewsChart []any `json:"viewsChart,omitempty"`
+	WatchTimeChart []any `json:"watchTimeChart,omitempty"`
+	CountryViewCounts []any `json:"countryViewCounts,omitempty"`
+	CountryWatchTime []any `json:"countryWatchTime,omitempty"`
+	EngagementScore int32 `json:"engagementScore,omitempty"`
+}
+
+// StreamVideoStatisticsOptions represents the options for Stream Video
+//
+// Bunny.net API docs: https://docs.bunny.net/reference/video_getvideostatistics
+
+type StreamVideoStatisticsOptions struct {
+	VideoLibraryId int64 `json:"libraryId,omitempty"`
+	DateFrom any `json:"dateFrom,omitempty"`
+	DateTo any `json:"dateTo,omitempty"`
+	Hourly bool `json:"hourly,omitempty"`
+	VideoGuid string `json:"videoGuid,omitempty"`
+}
+
+// Gets the Video Statistics with the given Library Id and Video Id
+//
+// Bunny.net API docs: https://docs.bunny.net/reference/video_getvideostatistics
+func (s *StreamService) Get(ctx context.Context, opts *StreamVideoStatisticsOptions) (*StreamVideoStatistics, error) {
+	path := fmt.Sprintf("library/%d/statistics", opts.VideoLibraryId)
+	return resourceGet[StreamVideo](ctx, s.client, path, nil)
+}
